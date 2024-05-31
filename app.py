e7131db8393bcb5ea9451be972f5a75dc8f43f38
Kailash <118871056+Kailxxz@users.noreply.github.com>
2024-05-31 14:09:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,40 @@
 import requests
 import json
-import streamlit as st
+import gradio as gr
 
-url = "http://localhost:11434/api/generate"
-headers = {
-    'Content-Type': 'application/json'
+url="http://localhost:11434/api/generate"
+
+headers={
+
+    'Content-Type':'application/json'
 }
-history = []
+
+history=[]
 
 def generate_response(prompt):
     history.append(prompt)
-    final_prompt = "\n".join(history)
-    data = {
-        "model": "codingsensei",
-        "prompt": final_prompt,
-        "stream": False
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+    final_prompt="\n".join(history)
 
-    if response.status_code == 200:
-        response = response.json()
-        actual_response = response['response']
+    data={
+        "model":"codingsensei",
+        "prompt":final_prompt,
+        "stream":False
+    }
+
+    response=requests.post(url,headers=headers,data=json.dumps(data))
+
+    if response.status_code==200:
+        response=response.text
+        data=json.loads(response)
+        actual_response=data['response']
         return actual_response
     else:
-        st.error(f"Error: {response.text}")
+        print("error:",response.text)
 
-def main():
-    st.title("Code assistant")
-    prompt = st.text_area("Enter your prompt", height=100)
-    if st.button("Submit"):
-        response = generate_response(prompt)
-        st.text_area("Response", value=response, height=100)
 
-if __name__ == "__main__":
-    main()
+interface=gr.Interface(
+    fn=generate_response,
+    inputs=gr.Textbox(lines=4,placeholder="Enter your Prompt"),
+    outputs="text"
+)
+interface.launch()
